0d2175cec23014cb3bf4b2229abeb78669629e7f
Vishalpraj12 <vishalprajapat7477@gmail.com>
2026-07-02 11:33:33 +0530

    Add files via upload

---
+++ chatmodel.py
@@ -0,0 +1,75 @@
+'''from dotenv import load_dotenv
+from langchain_google_genai import ChatGoogleGenerativeAI
+
+load_dotenv()
+
+llm = ChatGoogleGenerativeAI(
+    model="gemini-2.5-flash",
+    temperature=0.7
+)
+
+print("Type 'exit' to quit.\n")
+
+while True:
+    user_input = input("You: ")
+
+    if user_input.lower() == "exit":
+        break
+
+    response = llm.invoke(user_input)
+
+    print("AI:", response.content)'''
+
+from dotenv import load_dotenv
+from langchain_google_genai import ChatGoogleGenerativeAI
+import streamlit as st
+
+
+load_dotenv()
+
+
+st.set_page_config(
+    page_title="AI Chat Model",
+    page_icon="🤖",
+    layout="centered"
+)
+
+
+st.title("🤖 AI Chat Model")
+st.write("Powered by LangChain + Gemini 2.5 Flash")
+
+
+llm = ChatGoogleGenerativeAI(
+    model="gemini-2.5-flash",
+    temperature=0.7
+)
+
+# Store Chat History
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+
+prompt = st.chat_input("Type your message...")
+
+if prompt:
+    st.session_state.messages.append(
+        {"role": "user", "content": prompt}
+    )
+
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    
+    response = llm.invoke(prompt)
+
+    with st.chat_message("assistant"):
+        st.markdown(response.content)
+
+    st.session_state.messages.append(
+        {"role": "assistant", "content": response.content}
+    )
